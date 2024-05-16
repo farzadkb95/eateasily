b2c706449766d3630f7a853c4f1450d5092df743
milad nazari <mnrmilad@gmail.com>
2024-05-16 07:34:01 +0330

    create page 1

---
+++ resources/js/vue/components/InfoBox.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Icon } from "@iconify/vue/dist/iconify.js";
+</script>
+
+<template>
+  <div
+    class="my-10 bg-green-50 border border-green-500 rounded-md p-5 pt-8 text-green-950 relative"
+  >
+    <span
+      class="block absolute -top-6 w-12 aspect-square bg-green-500 border-4 border-white text-white rounded-full p-2"
+      ><Icon icon="solar:chat-round-line-outline" class="w-full h-full"
+    /></span>
+    <slot></slot>
+  </div>
+</template>
---
+++ resources/js/vue/components/QuestionBox.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="my-10 text-2xl md:text-[28px] font-bold text-[#121212] text-center"
+  >
+    <slot></slot>
+  </div>
+</template>
