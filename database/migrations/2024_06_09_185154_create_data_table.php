dd8e15592cf304b1c8835164e2e5c08ee9ffd671
milad nazari <mnrmilad@gmail.com>
2024-06-09 22:33:58 +0330

    add data tables migrations

---
+++ database/migrations/2024_06_09_185154_create_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('guest_id')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->unsignedTinyInteger('age')->nullable();
+            $table->unsignedTinyInteger('height')->nullable();
+            $table->unsignedTinyInteger('weight')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data');
+    }
+};
