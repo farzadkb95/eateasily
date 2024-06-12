1037bbcd546a680dd92c682b7fbcb86964485df2
milad nazari <mnrmilad@gmail.com>
2024-06-12 22:57:09 +0330

    change guest data resource

--- app/Http/Resources/GuestDataResource.php
+++ app/Http/Resources/GuestDataResource.php
@@ -14,6 +14,9 @@ class GuestDataResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'latest_test' => $this->latestTest,
+        ];
     }
 }
