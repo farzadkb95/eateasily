a5eaddffc99e5963084341c5ce67c12735f62b8c
milad nazari <mnrmilad@gmail.com>
2024-06-15 01:12:30 +0330

    remove unused code in raw layout

--- resources/js/vue/layouts/Raw.vue
+++ resources/js/vue/layouts/Raw.vue
@@ -1,23 +1,15 @@
 <script setup>
-import Btn from "@/components/Btn.vue";
-import ProgressBar from "@/components/ProgressBar.vue";
-import { Icon } from "@iconify/vue";
-
 import { useRoute, useRouter } from "vue-router";
-import { previousPageName, pageNumber, pagesCount } from "../modules/config";
+import { previousPageName } from "../modules/config";
 
 const route = useRoute();
 const router = useRouter();
-
-function previousPage() {
-  router.push({ name: previousPageName(route.name) });
-}
 </script>
 
 <template>
   <div class="mb-48">
     <header
-      class="shadow-emerald-100 shadow-lg flex items-center justify-center h-12"
+      class="shadow-emerald-100 shadow-lg flex items-center justify-center h-12 relative"
     >
       <div class="py-2 h-full">
         <img src="@/assets/logo.png" alt="" class="h-full" />
