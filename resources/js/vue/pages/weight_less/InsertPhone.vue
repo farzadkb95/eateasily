19b248f06968bb516aaace07eb60f8356ebfe70a
milad nazari <mnrmilad@gmail.com>
2024-07-15 23:56:24 +0330

    create mobile or email page

--- resources/js/vue/pages/weight_less/InsertPhone.vue
+++ resources/js/vue/pages/weight_less/InsertPhone.vue
@@ -7,11 +7,14 @@ import { useRoute, useRouter } from "vue-router";
 import { nextPageName } from "../../modules/config";
 import { ref, watchEffect } from "vue";
 import { useTestStore } from "../../store/TestStore";
+import { Icon } from "@iconify/vue";
 
 const route = useRoute();
 const router = useRouter();
 const mobile = ref(null);
+const email = ref(null);
 const testStore = useTestStore();
+const inside = ref(true);
 
 // watchEffect(() => {
 //   height.value = testStore.test?.height;
@@ -25,13 +28,80 @@ function operation() {
 <template>
   <Base>
     <div class="c-box">
-      <QuestionBox><p>شماره موبایل خود را وارد کنید</p></QuestionBox>
-
+      <QuestionBox><p>کجا زندگی می‌کنید؟</p></QuestionBox>
+      <div class="flex">
+        <div
+          class="w-1/2 flex items-center gap-2 justify-center cursor-pointer"
+          @click="inside = true"
+        >
+          <div
+            class="border rounded-full border-gray-400 p-[2px] w-6 h-6"
+            :class="{ '!border-pink-500': inside }"
+          >
+            <div
+              class="rounded-full bg-pink-500 w-full h-full"
+              v-if="inside"
+            ></div>
+          </div>
+          داخل کشور
+        </div>
+        <div
+          class="w-1/2 flex items-center gap-2 justify-center cursor-pointer"
+          @click="inside = false"
+        >
+          <div
+            class="border rounded-full border-gray-400 p-[2px] w-6 h-6"
+            :class="{ '!border-pink-500': !inside }"
+          >
+            <div
+              class="rounded-full bg-pink-500 w-full h-full"
+              v-if="!inside"
+            ></div>
+          </div>
+          خارج کشور
+        </div>
+      </div>
+      <hr class="my-5" />
       <form @submit.prevent="operation">
-        <Input placeholder="09......" v-model="mobile" dir="ltr" />
+        <div v-show="inside">
+          <small>شماره تماس</small>
+          <Input
+            class="!mt-0"
+            placeholder="09......"
+            v-model="mobile"
+            dir="ltr"
+          >
+            <template #start>
+              <Icon icon="solar:phone-linear" class="w-6 h-full" />
+            </template>
+          </Input>
+        </div>
+        <div v-show="!inside">
+          <div class="text-center my-5 text-lg">
+            آدرس ایمیل خود را وارد کنید
+          </div>
+          <small>ایمیل</small>
+          <Input
+            class="!mt-0"
+            placeholder="sample@gmail.com"
+            v-model="email"
+            dir="ltr"
+          >
+            <template #start>
+              <Icon icon="hugeicons:mail-02" class="w-6 h-full" />
+            </template>
+          </Input>
+        </div>
 
         <Btn class="w-full mt-6 !rounded-xl !h-14" type="submit">ثبت</Btn>
       </form>
+      <p
+        class="mt-5 p-5 border-green-500 bg-green-100 border rounded-lg"
+        v-show="inside"
+      >
+        برات آنالیز کـامل قراره پیامک بشه ، پس شماره‌ت رو وارد کن تا کلی اطلاعات
+        مفید راجع به شرایط بدنیت برات ارسال کنیم
+      </p>
     </div>
   </Base>
 </template>
