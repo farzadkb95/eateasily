38e13b61a5d60e1179326e39e44924e6728f6c62
milad nazari <mnrmilad@gmail.com>
2024-07-15 13:47:02 +0330

    add images to walking condition page

--- resources/js/vue/pages/weight_less/WalkingCondition.vue
+++ resources/js/vue/pages/weight_less/WalkingCondition.vue
@@ -67,5 +67,17 @@ function operation(index) {
         </ItemBox>
       </div>
     </div>
+    <img
+      v-if="testStore.test?.gender == 'male'"
+      src="@/assets/boy-q10.png"
+      class="h-52 md:h-60 fixed block bottom-0 left-10 md:translate-x-1/2 -z-10"
+      alt=""
+    />
+    <img
+      v-else
+      src="@/assets/girl-q10.png"
+      class="h-52 md:h-60 fixed block bottom-0 left-10 md:translate-x-1/2 -z-10"
+      alt=""
+    />
   </Base>
 </template>
