a823a199935d16db5088dc72927c8fc804c5f3e0
milad nazari <mnrmilad@gmail.com>
2024-07-15 23:26:06 +0330

    fix input to add icons

--- resources/js/vue/components/Input.vue
+++ resources/js/vue/components/Input.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { computed } from "vue";
+import { computed, useSlots } from "vue";
 
+const slots = useSlots();
 const props = defineProps({
   modelValue: null,
   type: {
     type: String,
     default: "text",
+  },
+  placeholder: {
+    type: String,
   },
 });
 const emit = defineEmits(["update:modelValue"]);
@@ -21,9 +25,15 @@ const value = computed({
 </script>
 
 <template>
-  <input
-    :type="props.type"
-    v-model="value"
-    class="border rounded-xl w-full h-14 px-2 outline-none mt-5"
-  />
+  <div class="flex items-center border rounded-xl mt-5 px-2 gap-2">
+    <template v-if="slots.start">
+      <slot name="start"></slot>
+    </template>
+    <input
+      :type="props.type"
+      :placeholder="props.placeholder"
+      v-model="value"
+      class="w-full h-14 outline-none bg-transparent"
+    />
+  </div>
 </template>
