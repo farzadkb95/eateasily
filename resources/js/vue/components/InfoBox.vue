c87e043c90672bd2077edff8b0eeedfd72d66525
milad nazari <mnrmilad@gmail.com>
2024-07-19 23:32:09 +0330

    fix box icon problem in iphone

--- resources/js/vue/components/InfoBox.vue
+++ resources/js/vue/components/InfoBox.vue
@@ -7,7 +7,7 @@ import { Icon } from "@iconify/vue";
     class="my-10 bg-green-50 border border-green-500 rounded-md p-5 pt-8 text-green-950 relative"
   >
     <span
-      class="block absolute -top-6 w-12 aspect-square bg-green-500 border-4 border-white text-white rounded-full p-2"
+      class="absolute -top-6 w-12 h-12 bg-green-500 border-4 border-white text-white rounded-full p-2"
       ><Icon icon="solar:chat-round-line-outline" class="w-full h-full"
     /></span>
     <slot></slot>
